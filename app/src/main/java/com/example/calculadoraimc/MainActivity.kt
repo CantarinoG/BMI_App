f3ad5ab3d7f0c4b8c7118cb63ac88264b4ed316d
Cantarino <guilhermecantarino1709@gmail.com>
2023-08-16 13:16:33 -0300

    Add main activity context class

--- app/src/main/java/com/example/calculadoraimc/MainActivity.kt
+++ app/src/main/java/com/example/calculadoraimc/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.calculadoraimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val editWeight: EditText = findViewById(R.id.editWeight)
+        val editHeight: EditText = findViewById(R.id.editHeight)
+        val btnCalc: Button = findViewById(R.id.btnCalc)
+
+        btnCalc.setOnClickListener {
+            val weight = editWeight.text.toString().toDoubleOrNull() ?: 0.0
+            val height = editHeight.text.toString().toDoubleOrNull() ?: 0.0
+            if(weight == 0.0) Toast.makeText(this, this.getString(R.string.nullWeight), Toast.LENGTH_LONG)
+            else if(height == 0.0) Toast.makeText(this, this.getString(R.string.nullHeight), Toast.LENGTH_LONG)
+            else {
+                val bmi = weight / (height * height)
+
+            }
+        }
+
     }
 }
