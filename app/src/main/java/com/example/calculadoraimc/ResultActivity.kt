ed781811443e3fed25b6466e409f5feb22395931
Cantarino <guilhermecantarino1709@gmail.com>
2023-08-18 11:38:24 -0300

    Add result output

--- app/src/main/java/com/example/calculadoraimc/ResultActivity.kt
+++ app/src/main/java/com/example/calculadoraimc/ResultActivity.kt
@@ -1,8 +1,10 @@
 package com.example.calculadoraimc
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 
 class ResultActivity : AppCompatActivity() {
@@ -10,10 +12,62 @@ class ResultActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
-        //val none: TextView = findViewById(R.id.none)
-        //none.text = intent.getDoubleExtra("bmi", 0.0).toString()
-        //a.text = intent.getDoubleExtra("bmi", 0.0)
-        //intent.getDoubleExtra("bmi", 0.0)
+        val bmi = intent.getDoubleExtra("bmi", 0.0)
+
+        val txtBmi: TextView = findViewById(R.id.txtBmi)
+        val txtResult: TextView = findViewById(R.id.txtResult)
+        val imgResult: ImageView = findViewById<ImageView>(R.id.imgResult)
+
+        when {
+            bmi < 16 -> {
+                imgResult.setImageResource(R.drawable.magreza_grave)
+                txtResult.text = getString(R.string.severeUnderweight)
+                txtResult.setTextColor(Color.parseColor("#0C4D8B"))
+                txtBmi.setTextColor(Color.parseColor("#0C4D8B"))
+            }
+            bmi >= 16 && bmi < 17 -> {
+                imgResult.setImageResource(R.drawable.magreza_moderada)
+                txtResult.text = getString(R.string.moderateUnderweight)
+                txtResult.setTextColor(Color.parseColor("#0377BE"))
+                txtBmi.setTextColor(Color.parseColor("#0377BE"))
+            }
+            bmi >= 17 && bmi < 18.5 -> {
+                imgResult.setImageResource(R.drawable.abaixo_do_peso)
+                txtResult.text = getString(R.string.underweight)
+                txtResult.setTextColor(Color.parseColor("#64A2D7"))
+                txtBmi.setTextColor(Color.parseColor("#64A2D7"))
+            }
+            bmi >= 18.5 && bmi < 25 -> {
+                imgResult.setImageResource(R.drawable.saudavel)
+                txtResult.text = getString(R.string.healthyWeight)
+                txtResult.setTextColor(Color.parseColor("#66BB6A"))
+                txtBmi.setTextColor(Color.parseColor("#66BB6A"))
+            }
+            bmi >= 25 && bmi < 30 -> {
+                imgResult.setImageResource(R.drawable.sobrepeso)
+                txtResult.text = getString(R.string.overweight)
+                txtResult.setTextColor(Color.parseColor("#FFFFC400"))
+                txtBmi.setTextColor(Color.parseColor("#FFFFC400"))
+            }
+            bmi >= 30 && bmi < 35 -> {
+                imgResult.setImageResource(R.drawable.obesidade_grau_i)
+                txtResult.text = getString(R.string.gradeIObesity)
+                txtResult.setTextColor(Color.parseColor("#F2683C"))
+                txtBmi.setTextColor(Color.parseColor("#F2683C"))
+            }
+            bmi >= 35 && bmi < 40 -> {
+                imgResult.setImageResource(R.drawable.obesidade_grau_ii)
+                txtResult.text = getString(R.string.gradeIIObesity)
+                txtResult.setTextColor(Color.parseColor("#BE3A26"))
+                txtBmi.setTextColor(Color.parseColor("#BE3A26"))
+            }
+            bmi >= 40 -> {
+                imgResult.setImageResource(R.drawable.obesidade_grau_iii)
+                txtResult.text = getString(R.string.gradeIIIObesity)
+                txtResult.setTextColor(Color.parseColor("#861619"))
+                txtBmi.setTextColor(Color.parseColor("#861619"))
+            }
+        }
 
     }
 }
